7d21e74b2bba564a08c69a62fc43fa1f30d7bf14
shunyue1320 <tsunyue1320@gmail.com>
2022-09-24 22:46:37 +0800

    chore: init

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,59 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, onMounted } from "vue";
+import { read, utils, writeFileXLSX } from "xlsx";
+
+const heads = ref(["品名", "单位", "单价"]);
+const rows = ref([]);
+
+const exportXLSX = async (e) => {
+  const file = e.target.files[0];
+  const data = await file.arrayBuffer();
+  const wb = read(data);
+  const ws = wb.Sheets[wb.SheetNames[0]];
+  console.log("utils.sheet_to_json(ws) ===", utils, utils.sheet_to_json(ws));
+  rows.value = utils.sheet_to_json(ws);
+  heads.value = Object.keys(rows.value[0]);
+  // setHTML(XLSX.utils.sheet_to_html(ws, { id: "tabeller" }));
+};
+
+// onMounted(async () => {
+//   /* Download from https://sheetjs.com/pres.numbers */
+//   const f = await fetch("https://sheetjs.com/pres.numbers");
+//   const ab = await f.arrayBuffer();
+
+//   /* parse workbook */
+//   const wb = read(ab);
+
+//   /* update data */
+//   rows.value = utils.sheet_to_json(wb.Sheets[wb.SheetNames[0]]);
+// });
+
+/* get state data and export to XLSX */
+function exportFile() {
+  exportHead = [];
+  const ws = utils.json_to_sheet(rows.value);
+  const wb = utils.book_new();
+  utils.book_append_sheet(wb, ws, "Data");
+  writeFileXLSX(wb, "发货单.xlsx");
+}
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <input type="file" @change="exportXLSX" />
+    <button @click="exportFile">下载</button>
+    <table>
+      <thead>
+        <th v-for="name in heads" :key="name">{{ name }}</th>
+      </thead>
+      <tbody>
+        <tr v-for="(row, idx) in rows" :key="idx">
+          <td v-for="name in heads" :key="name">{{ row[name] }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <!-- <HelloWorld msg="Vite + Vue" /> -->
 </template>
 
 <style scoped>
